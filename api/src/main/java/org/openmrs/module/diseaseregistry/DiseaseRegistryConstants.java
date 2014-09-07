2dcabb3bfe0cd70cf01151989f18ba934e5cddd1
Ha Truong <truonghatsts@gmail.com>
2014-09-07 21:08:45 +0700

    Added Property opdWard
    This property points to the concept of Disease Registry OPD ward

---
+++ api/src/main/java/org/openmrs/module/diseaseregistry/DiseaseRegistryConstants.java
@@ -0,0 +1,7 @@
+package org.openmrs.module.diseaseregistry;
+
+public class DiseaseRegistryConstants {	
+	
+	public static final String MODULE_ID = "diseaseregistry";
+	public static final String PROPERTY_OPD_WARD = MODULE_ID + ".opdWard";
+}
